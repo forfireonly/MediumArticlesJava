e2b3e840c9229378b9bf04bb3486f2a993a4289f
annascott <workwithpurpose@yahoo.com>
2021-04-14 11:16:40 -0600

    Procedural programming

---
+++ src/Procedural.java
@@ -0,0 +1,20 @@
+public class Procedural {
+    public static void main(String[] args) {
+        // write your code here
+        //declaring and initializing two numbers
+        int firstNumber = 8;
+        int secondNumber = 4;
+
+        //performing operations
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        int quotient = firstNumber / secondNumber;
+
+        //Displaying result
+        System.out.printf("%d + %d = %d\n", firstNumber, secondNumber, sum );
+        System.out.printf("%d - %d = %d\n", firstNumber, secondNumber, difference);
+        System.out.printf("%d x %d = %d\n", firstNumber, secondNumber, product);
+        System.out.printf("%d : %d = %d", firstNumber, secondNumber, quotient);
+    }
+}
