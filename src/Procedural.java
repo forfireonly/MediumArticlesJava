8dfd6ed1933558bb485770cbcbd19d13777cc59b
annascott <workwithpurpose@yahoo.com>
2021-04-14 12:14:13 -0600

    commiting previous files#	out/

--- src/Procedural.java
+++ src/Procedural.java
@@ -13,6 +13,7 @@ public class Procedural {
 
         //Displaying result
         System.out.printf("%d + %d = %d\n", firstNumber, secondNumber, sum );
+
         System.out.printf("%d - %d = %d\n", firstNumber, secondNumber, difference);
         System.out.printf("%d x %d = %d\n", firstNumber, secondNumber, product);
         System.out.printf("%d : %d = %d", firstNumber, secondNumber, quotient);
