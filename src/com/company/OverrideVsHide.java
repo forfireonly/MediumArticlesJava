cbacceaeeb1479a85c46a55fe7b9fce82ba01605
annascott <workwithpurpose@yahoo.com>
2021-09-09 12:43:19 -0600

    Overriding vs Hiding

--- src/com/company/OverrideVsHide.java
+++ src/com/company/OverrideVsHide.java
@@ -2,74 +2,78 @@ package com.company;
 
 public class OverrideVsHide {
     public int number;
-    String name;
+    //String name;
 
-    protected int returnNumber() {
+    /*protected int returnNumber() {
         return number;
-    }
+    }*/
 
-    void printGreeting() {
+    /*void printGreeting() {
         System.out.println("Hi");
-    }
+    }*/
 
-    void calculateSum(int a, int b) {
+    /*void calculateSum(int a, int b) {
+        System.out.println("result of addition is " + (a + b));
+    }*/
+
+    static void calculateSum(int a, int b) {
         System.out.println("result of addition is " + (a + b));
     }
 
-    static void personalizedGreeting(String name) {
-        System.out.println(name + " loves to code");
+    void printResult() {
+        calculateSum(1, 1);
     }
+
+
 }
 
 class ChildClass extends OverrideVsHide {
 
     public static void main(String... input) {
+
         ChildClass example = new ChildClass();
 
         //inheriting instance variable
-        System.out.println("number = "  + (example.number));
+        // System.out.println("number = "  + (example.number));
 
         //Inheriting instance method
-        System.out.println("Result of method = " + example.returnNumber());
+        //System.out.println("Result of method = " + example.returnNumber());
 
         //inheriting default method in the same package
-        example.printGreeting();
+        //example.printGreeting();
 
         //inheriting default variable in the same package
-        System.out.println(example.name);
+        //System.out.println(example.name);
 
         //calling overriden method
-        example.calculateSum(1,1);
+        //example.calculateSum(1,1);
+
+        //method call by the method in parent
+        //example.printResult();
 
 
-
-        //calling static parent method
-       OverrideVsHide.personalizedGreeting("Anna");
+        //calling static child method
+        calculateSum(1, 1);
 
         //calling hiden method
-        personalizedGreeting("Anna");
+        example.printResult();
+
 
         //reference variable of type Parent
-        OverrideVsHide ref = example;
 
         //calling overriden method
-        ref.calculateSum(1,1);
+
 
         //calling hidden method
-        ref.personalizedGreeting("Anna");
-        example.personalizedGreeting("Anna");
-
-
 
     }
 
     //Overriding method calculateSum declaread in parent class
-    void calculateSum(int a, int b) {
+    /*void calculateSum(int a, int b) {
         System.out.println("binary result of addition is " + Integer.toBinaryString(a + b));
-    }
+    }*/
 
-    //hiding the static parent method
-    static void personalizedGreeting(String name) {
-        System.out.println(name + " loves JAVA");
+    static void calculateSum(int a, int b) {
+        System.out.println("binary result of addition is " + Integer.toBinaryString(a + b));
     }
 }
