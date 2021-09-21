0502bf5188b193294b75852cb1fec715845cca71
annascott <workwithpurpose@yahoo.com>
2021-09-21 14:41:48 -0600

    Switch statement

---
+++ src/com/company/SwitchStatement.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class SwitchStatement {
+
+    public static void main(String... input){
+        String name = "Anna";
+        //final String name = "Anna";
+
+        switch("Anna"){
+            case "Boris":
+                System.out.println("My name is " + name);
+                break;
+            case name:
+                System.out.println("My name is " + name);
+            case "John":
+                System.out.println("I like JAVA!!!");
+            case "Mary":
+                System.out.println("Eat, sleep, code, repeat");
+                break;
+            case 5:
+                System.out.println();
+            default:
+        }
+    }
+}
